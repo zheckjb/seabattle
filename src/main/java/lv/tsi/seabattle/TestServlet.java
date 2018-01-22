ace69e3c8d7058997bee95994e3daa912cc4b078
zheckjb <zheck@inbox.lv>
2018-01-22 20:52:37 +0200

    Lesson 1

---
+++ src/main/java/lv/tsi/seabattle/TestServlet.java
@@ -0,0 +1,21 @@
+package lv.tsi.seabattle;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "TestServlet", urlPatterns = "/runonserver")
+public class TestServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Let's go home");
+        String name = request.getParameter("name");
+        response.getWriter().print("Let's go home, "+name+"!");
+    }
+}
